538181d5a84223d4c045b4df2e42842493d1ead5
LeonZz <1171734400@qq.com>
2022-05-10 23:39:55 +0800

    three num sum

--- src/ThreeSum.py
+++ src/ThreeSum.py
@@ -1,23 +1,41 @@
-def three_sum(inputs):
-    
-    n = len(inputs)
+def three_sum(nums):
     result = []
+    n = len(nums)
+    if n < 3:
+        return result
 
+    nums.sort()
 
-    for i,value in enumerate(inputs):
+    for i in range(0, n - 2):
+
+        if nums[i] > 0:
+            break
+
         left = i + 1
-        right = n -1
+        right = n - 1
+
+        if i > 0 and nums[i] == nums[i-1]:
+            continue
 
         while left < right:
-            sum =  inputs[left] + inputs[right] + inputs[i]
-            if sum == 0:
-                result.append(i,left,right)
+
+            if nums[i] + nums[left] + nums[right] == 0:
+                result.append([nums[i], nums[left], nums[right]])
+                right -= 1
                 left += 1
+                while nums[left] == nums[left - 1] and left < right:
+                    left += 1
+                while nums[right] == nums[right + 1] and left < right:
+                    right -= 1
+
+            elif nums[i] + nums[left] + nums[right] > 0:
                 right -= 1
 
-            elif sum < 0 :
+            elif nums[i] + nums[left] + nums[right] < 0:
                 left += 1
-            elif sum > 0 :
-                right -= 1
 
-        return result
+    return result
+
+
+if __name__ == "__main__":
+    print(three_sum([-4, -2, -2, -2, 0, 1, 2, 2, 2, 3, 3, 4, 4, 6, 6]))
