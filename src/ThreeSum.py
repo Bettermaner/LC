5d9223cc6e77f7c73d838adcd29c2c38c5bac580
liaoxuan <liaoxuan@netease.com>
2022-05-10 19:55:12 +0800

    update

---
+++ src/ThreeSum.py
@@ -0,0 +1,23 @@
+def three_sum(inputs):
+    
+    n = len(inputs)
+    result = []
+
+
+    for i,value in enumerate(inputs):
+        left = i + 1
+        right = n -1
+
+        while left < right:
+            sum =  inputs[left] + inputs[right] + inputs[i]
+            if sum == 0:
+                result.append(i,left,right)
+                left += 1
+                right -= 1
+                
+            elif sum < 0 and left < right:
+                left += 1
+            elif sum > 0 and left < right:
+                right -= 1
+
+        return result
