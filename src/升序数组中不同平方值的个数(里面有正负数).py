99df60a6d99015d280a1d3497f107af963cba6c5
unknown <leo.com>
2022-09-05 20:15:34 +0800

    update

---
+++ src/升序数组中不同平方值的个数(里面有正负数).py
@@ -0,0 +1,27 @@
+# 解题思路
+    # 双指针，还需要一个pre指针，记录前一个时刻的值
+    # 每次遍历都需要与pre值比较下，如果不相等就+1
+
+def func(array):
+    left = 0
+    right = len(array) -1
+
+    pre = -1
+    res = 0
+
+    while left < right:
+        if (abs(array[left]) > abs(array[right])):
+                if abs(array[left]) != pre:
+                    res += 1
+                    pre = abs(array[left])
+                left += 1
+        else:
+            if abs(array[right]) != pre:
+                res += 1 
+                pre = abs(array[right])
+            right -= 1
+
+    return res
+
+
+print(func([-9,-3,-1,1,1,1,3,9,9]))
