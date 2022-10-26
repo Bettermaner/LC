fe0b310eb5cc67e5eb15e33a3946c282934db4fd
unknown <leo@git.com>
2022-10-26 14:48:55 +0800

    updage

--- src/二叉树的中序遍历.py
+++ src/二叉树的中序遍历.py
@@ -16,3 +16,38 @@ def func(root):
     dfs(root,res)
     
     return res
+
+# 非递归（栈）
+# 中序遍历
+def in_order_func(root):
+
+    res = []
+    tmp = root
+    stack = []
+
+    while stack or tmp:
+        while tmp:
+            stack.append(tmp)
+            tmp = tmp.left
+
+        tmp = stack.pop()
+        res.append(tmp.val)
+        tmp = tmp.right
+
+    return res
+
+# 前序遍历
+def pre_order_func(root):
+
+    stack  =[]
+    res = []
+
+    while stack:
+        tmp = stack.pop()
+        res.append(tmp.val)
+        if tmp.right:
+            stack.append(tmp.right)
+        if tmp.left:
+            stack.append(tmp.left)
+
+    return res 
