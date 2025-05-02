317c6eead4f9869087ee9f7f51fff808715bfef0
leo07.liao <leo07.liao@vipshop.com>
2025-05-03 00:41:57 +0800

    update

--- src/二叉树-根节点到叶子节点数字之和.py
+++ src/二叉树-根节点到叶子节点数字之和.py
@@ -43,33 +43,3 @@ class Solution:
         dfs(root, 0)
         return self.total
     
-# 示例：
-
-# 输入：root = [1,2,3]
-
-# 输出：25
-
-# 解题思路
-    # 递归
-
-
-def func(root):
-
-    if not root:
-        return 0
-
-    total = dfs(root,0)
-    return total
-
-
-def dfs(root,total):
-    if not root:
-        return 0
-
-    total = total * 10 + root.val
-
-    if not root.left and not root.right:
-        return total
-
-    else:
-        return dfs(root.left,total) + dfs(root.right,total)
